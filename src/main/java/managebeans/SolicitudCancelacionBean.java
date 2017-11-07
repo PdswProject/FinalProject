53d6bef011698a9b2f448ffd2f140aab02dec868
d.castiblanco <dolardanies@hotmail.com>
2017-11-07 11:34:25 -0500

    Prueba estandarizacion nombre

--- src/main/java/managebeans/SolicitudCancelacionBean.java
+++ src/main/java/managebeans/SolicitudCancelacionBean.java
@@ -5,6 +5,9 @@
  */
 package managebeans;
 
+import entities.Asignatura;
+import java.util.Date;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 /**
@@ -12,9 +15,49 @@ import javax.faces.bean.SessionScoped;
  * @author USER
  */
 
-@ManagedBean(name = "HistorialPacientes")
+@ManagedBean(name = "Solicitudes")
 @SessionScoped
 
 public class SolicitudCancelacionBean {
+    int id;
+    int estudiante;
+    int asignatura;
+    Date fecha;
+    String estado;
+    int decano;
+    
+    
+    public SolicitudCancelacionBean(){
+        
+    }
+    
+    /**
+     * Algoritmo de cálculo de impacto que se limita
+       a indicar, dada la asignatura a cancelar, 
+       cuantos créditos académicos tendría pendiente por ver.
+     * @param asig
+     * @return 
+     */
+    public int calculoImpacto(Asignatura asig){
+        return 0;
+        
+    }
+    
+    
+    /**
+     * El sistema debe permitir seleccionar sólo 
+     * aquellas asignaturas que esté actualmente 
+     * viendo el usuario.
+     * @return 
+     */
+    public List<Asignatura> loadAsignaturas(){
+        return null;
+    }
+    
+    public void registroJustificacion(){
+        
+    }
+    
+    
     
 }
