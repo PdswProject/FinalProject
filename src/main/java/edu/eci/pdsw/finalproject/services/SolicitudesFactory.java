caae38db43e08e83700e1d56271bb68707e84679
d.beltran <bleachstick@hotmail.com>
2017-11-09 12:52:36 -0500

    Configuración Base de datos en Heroku

--- src/main/java/edu/eci/pdsw/finalproject/services/SolicitudesFactory.java
+++ src/main/java/edu/eci/pdsw/finalproject/services/SolicitudesFactory.java
@@ -5,10 +5,62 @@
  */
 package edu.eci.pdsw.finalproject.services;
 
+import com.google.inject.AbstractModule;
+import static com.google.inject.Guice.createInjector;
+import com.google.inject.Injector;
+import org.mybatis.guice.XMLMyBatisModule;
+import org.mybatis.guice.datasource.helper.JdbcHelper;
+
 /**
  *
  * @author USER
  */
 public class SolicitudesFactory {
+    private static SolicitudesFactory instance = new SolicitudesFactory();
+
+    private static Injector injector;
     
+    private static Injector testInjector;
+
+    public SolicitudesFactory() {
+        
+         injector = createInjector(new XMLMyBatisModule() {
+
+            @Override
+            protected void initialize() {
+                install(JdbcHelper.PostgreSQL);              
+                setClassPathResource("mybatis-config.xml");
+                
+            }
+
+        }
+        );
+
+        testInjector = createInjector(new XMLMyBatisModule() {
+
+            @Override
+            protected void initialize() {
+                install(JdbcHelper.PostgreSQL);
+                setClassPathResource("mybatis-config-h2.xml");
+                
+            }
+
+        }
+        );
+
+    }
+
+    public Solicitudes getSolicitudes() {
+        return injector.getInstance(Solicitudes.class);
+    }
+
+    public Solicitudes getTestingSolicitudes() {
+        return testInjector.getInstance(Solicitudes.class);
+    }
+    
+    public static SolicitudesFactory getInstance() {
+        return instance;
+    }
+
 }
+
