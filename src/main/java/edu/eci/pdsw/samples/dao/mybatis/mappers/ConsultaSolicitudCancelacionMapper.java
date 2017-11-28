439e2a544d019b7f8cabada91ca2194c5602c0e6
cesareduardo824 <cesareduardo824@gmail.com>
2017-11-28 01:26:03 -0500

    Problema solucionado

--- src/main/java/edu/eci/pdsw/samples/dao/mybatis/mappers/ConsultaSolicitudCancelacionMapper.java
+++ src/main/java/edu/eci/pdsw/samples/dao/mybatis/mappers/ConsultaSolicitudCancelacionMapper.java
@@ -14,6 +14,6 @@ import java.util.List;
  * @author cesar
  */
 public interface ConsultaSolicitudCancelacionMapper {
-        public List<SolicitudCancelacion> getSolictudCancelacion();
+        public List<SolicitudCancelacion> loadSolictudCancelacion();
     
 }
